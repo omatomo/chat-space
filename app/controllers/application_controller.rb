77913e003ba60ce736d6f18d3b68aa78527f08b1
omatomo <ajadj4@gmail.com>
2017-02-25 01:55:11 +0900

    change the way of permitting column name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_aciton :configure_permitted_parameters, if: :devise_controller?
+protect_from_forgery with: :exception
+before_action :configure_permitted_parameters, if: :devise_controller?
 
-private
-	def configure_permitted_parameters
-	  devise_parameter_sanitizer.for(:sign_up) << :name
-	end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
 end
 
