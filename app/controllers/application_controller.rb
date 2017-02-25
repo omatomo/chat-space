bc6b9377048a5216e6b8efe11f3666c78e90b968
omatomo <ajadj4@gmail.com>
2017-02-25 16:21:53 +0900

    nickname to name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ protect_from_forgery with: :exception
 before_action :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
 
