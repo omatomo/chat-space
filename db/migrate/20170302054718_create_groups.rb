b731b353b0101c497d53893dca505a264e4baade
omatomo <ajadj4@gmail.com>
2017-03-04 14:47:35 +0900

    :name to :group_name

--- db/migrate/20170302054718_create_groups.rb
+++ db/migrate/20170302054718_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :name, null: false, index: true
+      t.string :group_name, null: false, index: true
 
       t.timestamps null: false
     end
