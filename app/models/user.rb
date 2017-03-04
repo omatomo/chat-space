174225c0972dcbcc22ddb4c79cc16c5ef0b97d26
omatomo <ajadj4@gmail.com>
2017-03-04 14:46:25 +0900

    association in model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+
+  has_many :users_groups
+  has_many :groups, through: :users_groups, dependent: :destroy
 end
