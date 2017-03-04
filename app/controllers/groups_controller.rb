a62e7121bc281571f1e5ae5790dd88cf37111ad1
omatomo <ajadj4@gmail.com>
2017-03-04 14:47:19 +0900

    groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,23 @@ class GroupsController < ApplicationController
   end
 
   def create
+   group = Group.new(group_params)
+   group.save
+   redirect_to controller: 'chats', action: 'index'
   end
 
   def edit
+  	@group = Group.find(params[:id])
   end
 
   def update
+  	group = Group.find(params[:id])
+  	group.update(group_params)
+  	redirect_to controller: 'chats', action: 'index'
+  end
+
+  private
+  def group_params
+  	params.require(:group).permit(:group_name)
   end
 end
