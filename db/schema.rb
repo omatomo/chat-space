941754c9bd16798d833760cd2b9cbf679bd9e6d0
omatomo <ajadj4@gmail.com>
2017-03-04 14:45:33 +0900

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302054718) do
+ActiveRecord::Schema.define(version: 20170304034831) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
+    t.string   "group_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
+    t.index ["group_name"], name: "index_groups_on_group_name", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -38,4 +38,15 @@ ActiveRecord::Schema.define(version: 20170302054718) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "users_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_users_groups_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_users_groups_on_user_id", using: :btree
+  end
+
+  add_foreign_key "users_groups", "groups"
+  add_foreign_key "users_groups", "users"
 end
